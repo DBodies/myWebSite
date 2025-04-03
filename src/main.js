dddd647707ed016c369a04abc66ba229a342aef1
Denis <dkovalevskiy02@gmail.com>
2025-04-03 18:45:08 +0300

    update resume

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { langSwitcher } from "./js/languageSwitcher";
-import { setupResume } from "./js/resumeDownload"; 
 import { notification } from "./js/izitoast";
 import { sendMessageToEmail } from "./js/sendMessage"; 
 
 
 window.addEventListener('DOMContentLoaded', async () => {
     langSwitcher();
-    setupResume()
     notification()
     sendMessageToEmail()
 });
